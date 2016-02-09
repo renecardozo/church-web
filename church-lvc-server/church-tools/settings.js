e95e2b08647f43d2cb14ae93749dd8467d883c89
renecardozo <rene.cardozo.2014@gmail.com>
2016-02-09 00:44:05 -0430

    was solved issues related with third party library

--- church-lvc-server/church-tools/settings.js
+++ church-lvc-server/church-tools/settings.js
@@ -5,9 +5,13 @@ const db = {
 const connection = {
 	'port': process.env.PORT || 8080
 }
+const client_connection = {
+    folder: '../church-lvc-client'
+}
 
 const configuration = {
 	'db': db,
-	'connection': connection
+	'connection': connection,
+    'client_connection': client_connection 
 }
 module.exports = configuration;
--- church-lvc-server/server.js
+++ church-lvc-server/server.js
@@ -4,17 +4,18 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose = require('mongoose');
 var settings = require('./church-tools');
-console.log(settings.connection.port);
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(methodOverride());
+app.use(express.static(settings.client_connection.folder));
+console.log(settings.client_connection.folder);
 
 var router = express.Router();
 
-router.get('/', function (req, res){
-	res.send('Hello World');
-});
+/*router.get('/', function (req, res){
+	res.sendfile(settings.client_connection.folder + 'index.html');
+});*/
 
 app.use(router);
 app.listen(settings.connection.port, function(){
